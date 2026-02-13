304e62d3482e296309791777e479df0ef0910766
Syahid Muhammad <syahidmuhammad@Syahids-MacBook-Air.local>
2026-02-13 18:49:14 +0700

    Enable prefix for default locale in i18n routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
     defaultLocale: "en",
     locales: ["en", "id"],
     routing: {
-      prefixDefaultLocale: false
+      prefixDefaultLocale: true
     }
   }
 })
